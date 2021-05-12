dc3892323283df0f9c38274e1372c8adfc687d15
emekarr <aceanyanwu02@gmail.com>
2021-05-12 15:58:30 +0100

    added env variables for mongodb uri and added an extra end point for wrong urls entered

--- app/models/mongodb.js
+++ app/models/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const start_mongo = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/zuri_project_1", {
+  mongoose.connect(process.env.MONGOKEY, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
--- app/server.js
+++ app/server.js
@@ -11,5 +11,8 @@ server.use(express.urlencoded({ extended: true }));
 // route to interact with the user model
 server.use("/user", require("./routes/user_route.js"));
 
+// incase someone uses a wrong endpoint
+server.use("*", (req, res) => {res.send("This endpoint does not exist!")})
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);
